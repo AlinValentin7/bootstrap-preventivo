78a4b6e4452b4db84252c3e1594e7cd44baa4e95
AlinValentin7 <trandafiralionvalentin@gmail.com>
2024-07-22 23:15:43 +0200

    Add: tolto prezzo dalla pagina

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
 
 
 
-    const preventivoElement = document.getElementById('preventivo');
-    // Ottiene l'elemento con l'ID 'preventivo'
-    const unitsElement = document.getElementById('units');
-    // Ottiene l'elemento che mostra il prezzo finale
-    const validFeedbackElement = document.querySelector('.valid-feedback');
-    // Ottiene l'elemento che mostra il feedback per un codice promozionale valido
-    const invalidFeedbackElement = document.querySelector('.invalid-feedback');
-    // Ottiene l'elemento che mostra il feedback per un codice promozionale non valido
-    const decimaleElement = document.querySelector('decimale');
+const preventivoElement = document.getElementById('preventivo');
+// Ottiene l'elemento con l'ID 'preventivo'
+const unitsElement = document.getElementById('units');
+// Ottiene l'elemento che mostra il prezzo finale
+const validFeedbackElement = document.querySelector('.valid-feedback');
+// Ottiene l'elemento che mostra il feedback per un codice promozionale valido
+const invalidFeedbackElement = document.querySelector('.invalid-feedback');
+// Ottiene l'elemento che mostra il feedback per un codice promozionale non valido
+const decimaleElement = document.querySelector('.decimale');
 
-    const prezzo = {
-        backend: 20.50,
-        frontend: 15.30,
-        project: 33.60
-    };
-    // Oggetto che memorizza i prezzi orari per ciascun tipo di lavoro
+const prezzo = {
+    backend: 20.50,
+    frontend: 15.30,
+    project: 33.60
+};
+// Oggetto che memorizza i prezzi orari per ciascun tipo di lavoro
 
-    const promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
-    // Array di codici promozionali validi
-    const discount = 0.25;
-    // Percentuale di sconto applicabile se il codice promozionale è valido
+const promoCodes = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
+// Array di codici promozionali validi
+const discount = 0.25;
+// Percentuale di sconto applicabile se il codice promozionale è valido
 
-    preventivoElement.addEventListener('submit', function(event) {
-        event.preventDefault();
-        // Previene l'invio del form che causerebbe un ricaricamento della pagina
+preventivoElement.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Previene l'invio del form che causerebbe un ricaricamento della pagina
 
-        const lavoroElement = document.getElementById('lavoro').value;
-        // Ottiene il valore selezionato nel campo di selezione del tipo di lavoro
-        const promoCodeElement = document.getElementById('promo').value.trim();
-        // Ottiene il valore del codice promozionale, eliminando eventuali spazi bianchi
+    const lavoroElement = document.getElementById('lavoro').value;
+    // Ottiene il valore selezionato nel campo di selezione del tipo di lavoro
+    const promoCodeElement = document.getElementById('promo').value.trim();
+    // Ottiene il valore del codice promozionale, eliminando eventuali spazi bianchi
 
-        if (!lavoroElement) {
-            alert('Per favore, seleziona un tipo di lavoro.');
-            // Se nessun tipo di lavoro è stato selezionato, mostra un messaggio di avviso e interrompe l'esecuzione della funzione
-            return;
-        }
+    if (!lavoroElement) {
+        alert('Per favore, seleziona un tipo di lavoro.');
+        // Se nessun tipo di lavoro è stato selezionato, mostra un messaggio di avviso e interrompe l'esecuzione della funzione
+        return;
+    }
 
-        let prezzoOrario = prezzo[lavoroElement];
-        // Ottiene il prezzo orario corrispondente al tipo di lavoro selezionato
-        let prezzoFinale = 10 * prezzoOrario;
-        // Calcola il prezzo finale supponendo che ogni progetto richieda 10 ore di lavoro
+    let prezzoOrario = prezzo[lavoroElement];
+    // Ottiene il prezzo orario corrispondente al tipo di lavoro selezionato
+    let prezzoFinale = 10 * prezzoOrario;
+    // Calcola il prezzo finale supponendo che ogni progetto richieda 10 ore di lavoro
 
-        if (promoCodes.includes(promoCodeElement)) {
-            prezzoFinale *= (1 - discount);
-            // Se il codice promozionale è valido, applica lo sconto del 25% al prezzo finale
-            validFeedbackElement.style.display = 'block';
-            invalidFeedbackElement.style.display = 'none';
-            // Mostra il feedback per il codice promozionale valido e nasconde quello per il codice non valido
-        } else if (promoCodeElement) {
-            validFeedbackElement.style.display = 'none';
-            invalidFeedbackElement.style.display = 'block';
-            // Se il codice promozionale è stato inserito ma non è valido, mostra il feedback per il codice non valido e nasconde quello per il codice valido
-        } else {
-            validFeedbackElement.style.display = 'none';
-            invalidFeedbackElement.style.display = 'none';
-            // Se non è stato inserito alcun codice promozionale, nasconde entrambi i feedback
-        }
-          // Formatta il prezzo con due cifre decimali e lo divide in parte intera e decimale
-          const [intero, decimali] = prezzoFinale.toFixed(2).split('.');
-          // Assegna la parte intera al contenuto di unitsElement
-          unitsElement.textContent = intero;
-          // Assegna la parte decimale al contenuto di decimalsElement
-          decimaleElement.textContent = `,${decimali}`;
-        
-        
-    });
+    if (promoCodes.includes(promoCodeElement)) {
+        prezzoFinale *= (1 - discount);
+        // Se il codice promozionale è valido, applica lo sconto del 25% al prezzo finale
+        validFeedbackElement.style.display = 'block';
+        invalidFeedbackElement.style.display = 'none';
+        // Mostra il feedback per il codice promozionale valido e nasconde quello per il codice non valido
+    } else if (promoCodeElement) {
+        validFeedbackElement.style.display = 'none';
+        invalidFeedbackElement.style.display = 'block';
+        // Se il codice promozionale è stato inserito ma non è valido, mostra il feedback per il codice non valido e nasconde quello per il codice valido
+    } else {
+        validFeedbackElement.style.display = 'none';
+        invalidFeedbackElement.style.display = 'none';
+        // Se non è stato inserito alcun codice promozionale, nasconde entrambi i feedback
+    }
+    // Formatta il prezzo con due cifre decimali e lo divide in parte intera e decimale
+    const [intero, decimali] = prezzoFinale.toFixed(2).split('.');
+    // Assegna la parte intera al contenuto di unitsElement (con textcontent)
+    unitsElement.textContent = intero;
+    // Assegna la parte decimale al contenuto di decimalsElement
+    decimaleElement.textContent = `,${decimali}`;
+
+
+});
 
